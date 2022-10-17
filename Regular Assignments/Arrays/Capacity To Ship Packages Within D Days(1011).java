899ba5aec0361ca1c432715955d4ca8d435aed79
Dhivyashreesk <107365369+Dhivyashreesk@users.noreply.github.com>
2022-10-17 23:12:21 +0530

    Create Capacity To Ship Packages Within D Days(1011).java

---
+++ Regular Assignments/Arrays/Capacity To Ship Packages Within D Days(1011).java
@@ -0,0 +1,25 @@
+class Solution {
+    public int shipWithinDays(int[] ws, int D) {
+        int left = 0, right = 0;
+        for (int w : ws) {
+            left = Math.max(w, left);
+            right += w;
+        }
+        while (left < right) {
+            int mid = (left + right) /2, curr = 0, days = 1; // days = 1;
+            for (int w : ws) {
+                if (curr + w > mid) {
+                    curr = 0;
+                    days++;
+                }
+                curr += w;
+            }
+            if (days > D) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
