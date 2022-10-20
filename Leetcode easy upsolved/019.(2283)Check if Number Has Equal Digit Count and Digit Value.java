3a8da22dd70bf766ffa67e54ca475ae2b08ed77a
Dhivyashreesk <107365369+Dhivyashreesk@users.noreply.github.com>
2022-10-20 19:10:23 +0530

    Create 019.(2283)Check if Number Has Equal Digit Count and Digit Value.java

---
+++ Leetcode easy upsolved/019.(2283)Check if Number Has Equal Digit Count and Digit Value.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean digitCount(String num) {
+        int freq[]=new int[10];
+        for(int i=0;i<num.length();i++){
+            freq[num.charAt(i)-'0']++;
+        }
+        for(int i=0;i<num.length();i++){
+            int n=num.charAt(i)-'0';
+            if(freq[i]!=n){
+                return false;
+            }
+        }
+        return true;
+    }
+}
