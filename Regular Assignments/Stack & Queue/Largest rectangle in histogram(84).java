a72ac1c8c79255add875482e31064950f8a9a41a
Dhivyashreesk <107365369+Dhivyashreesk@users.noreply.github.com>
2022-11-02 18:24:26 +0530

    Update Largest rectangle in histogram(84).java

--- Regular Assignments/Stack & Queue/Largest rectangle in histogram(84).java
+++ Regular Assignments/Stack & Queue/Largest rectangle in histogram(84).java
@@ -1,34 +1,16 @@
-public static int largestRectangleArea(int[] height) {
-    if (height == null || height.length == 0) {
-        return 0;
-    }
-    int[] lessFromLeft = new int[height.length]; // idx of the first bar the left that is lower than current
-    int[] lessFromRight = new int[height.length]; // idx of the first bar the right that is lower than current
-    lessFromRight[height.length - 1] = height.length;
-    lessFromLeft[0] = -1;
-
-    for (int i = 1; i < height.length; i++) {
-        int p = i - 1;
-
-        while (p >= 0 && height[p] >= height[i]) {
-            p = lessFromLeft[p];
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if (heights.length == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxArea = 0;
+        for (int i = 0; i <= heights.length; i++) {
+            int cur = i == heights.length ? 0 : heights[i];
+            while (stack.peek() != -1 && cur < heights[stack.peek()]) {
+                maxArea = Math.max(maxArea, heights[stack.pop()] * (i - stack.peek() - 1));
+            }
+            stack.push(i);
         }
-        lessFromLeft[i] = p;
+        return maxArea;
     }
-
-    for (int i = height.length - 2; i >= 0; i--) {
-        int p = i + 1;
-
-        while (p < height.length && height[p] >= height[i]) {
-            p = lessFromRight[p];
-        }
-        lessFromRight[i] = p;
-    }
-
-    int maxArea = 0;
-    for (int i = 0; i < height.length; i++) {
-        maxArea = Math.max(maxArea, height[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
-    }
-
-    return maxArea;
 }
